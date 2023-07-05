54bae464d5bfb5aa99c36ae37c3f1e0b6c521f5d
vitormoschetta <vitormoschetta@gmail.com>
2023-07-05 00:53:36 -0400

    add logger request and response

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/vitormoschetta/go
 go 1.20
 
 require (
+	github.com/go-chi/chi/v5 v5.0.8
 	github.com/go-sql-driver/mysql v1.7.0
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.5.1
 	github.com/stretchr/testify v1.8.2
 	github.com/swaggo/swag v1.16.1
+	go.uber.org/zap v1.24.0
 )
 
 require (
@@ -25,6 +27,8 @@ require (
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
--- pkg/middlewares/logging.go
+++ pkg/middlewares/logging.go
@@ -1,18 +1,62 @@
 package middlewares
 
 import (
-	"log"
+	"bytes"
+	"io"
 	"net/http"
-	"time"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func LoggingHandling(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		timeStart := time.Now()
-		traceID := r.Context().Value(CorrelationKey).(string)
-		log.Printf("%s Método: %s | Rota: %s\n", traceID, r.Method, r.URL.Path)
-		next.ServeHTTP(w, r)
-		timeElapsed := time.Since(timeStart)
-		log.Printf("%s Tempo de execução: %s\n", traceID, timeElapsed)
+		var reqBuf *bytes.Buffer
+		contentLength := int(r.ContentLength)
+		reqBuf = bytes.NewBuffer(make([]byte, 0, contentLength))
+		origBody := r.Body
+		defer origBody.Close()
+		r.Body = io.NopCloser(io.TeeReader(origBody, reqBuf))
+
+		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+		var resBuf *bytes.Buffer
+		resBuf = bytes.NewBuffer(make([]byte, 0, 1024))
+		ww.Tee(resBuf)
+
+		next.ServeHTTP(ww, r)
+
+		fields := []zapcore.Field{
+			zap.String("method", r.Method),
+			zap.String("url", r.URL.String()),
+			zap.Int("status", ww.Status()),
+		}
+
+		if reqBuf != nil {
+			fields = append(
+				fields,
+				zap.Reflect("request_headers", r.Header),
+				zap.ByteString("request_body", reqBuf.Bytes()),
+			)
+		}
+
+		if resBuf != nil {
+			fields = append(
+				fields,
+				zap.Reflect("response_headers", ww.Header()),
+				zap.ByteString("response_body", resBuf.Bytes()),
+			)
+		}
+
+		config := zap.Config{
+			Encoding:         "json",
+			Level:            zap.NewAtomicLevelAt(zapcore.DebugLevel),
+			OutputPaths:      []string{"stdout"},
+			ErrorOutputPaths: []string{"stderr"},
+		}
+
+		logger, _ := config.Build()
+		zap.ReplaceGlobals(logger)
+		zap.L().Debug("request", fields...)
 	})
 }
